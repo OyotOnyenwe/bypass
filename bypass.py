4e15f63e72e3c187d1704bc5f3aac226c3e1e821
Oyot Onyenwe <71685036+OyotOnyenwe@users.noreply.github.com>
2022-06-05 23:43:54 -0400

    add code comments

--- bypass.py
+++ bypass.py
@@ -1,12 +1,13 @@
 # The O-Ring Bypass, Automated
 # by Oyot Onyenwe
-# v1.1.0
+# v1.1.1
 
 import tkinter as tk
 import subprocess as sp
 from time import sleep
 
-def nameget():
+# Figure out which hardware port Wi-Fi runs on
+def name_get():
     devlist = str(sp.run(["networksetup", "-listallhardwareports"], stdout = sp.PIPE))
     i1 = devlist.find("Wi-Fi") + 15
     cutlist = devlist[i1:]
@@ -14,15 +15,17 @@ def nameget():
     name = cutlist[:i2]
     return name
 
+# Disable the port, then reenable
 def bypass():
-    name = nameget()
+    name = name_get()
     sp.run(["networksetup", "-setairportpower", name, "off"])
     sleep(1)
     sp.run(["networksetup", "-setairportpower", name, "on"])
     sleep(1)
 
+# Tkinter GUI Code
 window = tk.Tk()
-window.title("The O-Ring Bypasser")
+window.title("O-Ring Bypasser v1.1.1")
 
 tk.Label(text = "").grid(row = 0, column = 0)
 tk.Label(text = "Click to perform the Bypass:").grid(row = 1, column = 0)
